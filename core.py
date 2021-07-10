9390a2bcc26a818662ebd316b26af81aa54b7338
masihtamsoy <masihtamsoy@gmail.com>
2021-07-10 15:59:11 +0530

    Add pids to products list

--- core.py
+++ core.py
@@ -1,5 +1,5 @@
 
-# component
+# test html component
 html = """
 <div>
   <div data-id="TEAETC92QASWGFTV" style="width: 25%;">
@@ -39,70 +39,19 @@ html = """
       </div>
    </div>
   </div>
-
-  <div data-id="TEAETC92X9NXPDWE" style="width: 25%;">
-   <div class="_4ddWXP" data-tkid="c7cc3b99-8d53-4d0d-ae95-f87e28b3e0d8.TEAETC92X9NXPDWE.SEARCH">
-      <a class="_2rpwqI" target="_blank" rel="noopener noreferrer" href="/lipton-pure-light-green-tea-bags-box/p/itmex8erszekvngc?pid=TEAETC92X9NXPDWE&amp;lid=LSTTEAETC92X9NXPDWEJRM1L1&amp;marketplace=FLIPKART&amp;q=chai+&amp;store=eat%2Ffpm&amp;srno=s_1_13&amp;otracker=search&amp;otracker1=search&amp;fm=SEARCH&amp;iid=c7cc3b99-8d53-4d0d-ae95-f87e28b3e0d8.TEAETC92X9NXPDWE.SEARCH&amp;ppt=sp&amp;ppn=sp&amp;qH=65d26c2d2647ea6e">
-         <div>
-            <div>
-               <div class="CXW8mj _21_khk" style="height: 230px; width: 230px;"><img class="_396cs4 _3exPp9" alt="Lipton Pure &amp; Light Green Tea Bags Box" src="https://rukminim1.flixcart.com/image/612/612/kkbh8cw0/tea/d/w/k/pure-light-box-green-tea-tea-bag-lipton-original-imafzzp8rxq3kjsg.jpeg?q=70"></div>
-            </div>
-         </div>
-         <div class="_2hVSre _1eAP-x">
-            <div class="_36FSn5">
-               <svg xmlns="http://www.w3.org/2000/svg" class="_1l0elc" width="16" height="16" viewBox="0 0 20 16">
-                  <path d="M8.695 16.682C4.06 12.382 1 9.536 1 6.065 1 3.219 3.178 1 5.95 1c1.566 0 3.069.746 4.05 1.915C10.981 1.745 12.484 1 14.05 1 16.822 1 19 3.22 19 6.065c0 3.471-3.06 6.316-7.695 10.617L10 17.897l-1.305-1.215z" fill="#2874F0" class="eX72wL" stroke="#FFF" fill-rule="evenodd" opacity=".9"></path>
-               </svg>
-            </div>
-         </div>
-      </a>
-      <a class="s1Q9rs" title="Lipton Pure &amp; Light Green Tea Bags Box" target="_blank" rel="noopener noreferrer" href="/lipton-pure-light-green-tea-bags-box/p/itmex8erszekvngc?pid=TEAETC92X9NXPDWE&amp;lid=LSTTEAETC92X9NXPDWEJRM1L1&amp;marketplace=FLIPKART&amp;q=chai+&amp;store=eat%2Ffpm&amp;srno=s_1_13&amp;otracker=search&amp;otracker1=search&amp;fm=SEARCH&amp;iid=c7cc3b99-8d53-4d0d-ae95-f87e28b3e0d8.TEAETC92X9NXPDWE.SEARCH&amp;ppt=sp&amp;ppn=sp&amp;qH=65d26c2d2647ea6e">Lipton Pure &amp; Light Green Tea Bags Box</a>
-      <div class="_3Djpdu">25 Bags</div>
-      <div class="gUuXy- _2D5lwg">
-         <span id="productRating_LSTTEAETC92X9NXPDWEJRM1L1_TEAETC92X9NXPDWE_" class="_1lRcqv">
-            <div class="_3LWZlK">4.4<img src="data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHdpZHRoPSIxMyIgaGVpZ2h0PSIxMiI+PHBhdGggZmlsbD0iI0ZGRiIgZD0iTTYuNSA5LjQzOWwtMy42NzQgMi4yMy45NC00LjI2LTMuMjEtMi44ODMgNC4yNTQtLjQwNEw2LjUuMTEybDEuNjkgNC4wMSA0LjI1NC40MDQtMy4yMSAyLjg4Mi45NCA0LjI2eiIvPjwvc3ZnPg==" class="_1wB99o"></div>
-         </span>
-         <span class="_2_R_DZ">(20,494)</span>
-      </div>
-      <div class="_32g5_j"><img height="21" src="//static-assets-web.flixcart.com/www/linchpin/fk-cp-zion/img/fa_62673a.png"></div>
-      <a class="_8VNy32" target="_blank" rel="noopener noreferrer" href="/lipton-pure-light-green-tea-bags-box/p/itmex8erszekvngc?pid=TEAETC92X9NXPDWE&amp;lid=LSTTEAETC92X9NXPDWEJRM1L1&amp;marketplace=FLIPKART&amp;q=chai+&amp;store=eat%2Ffpm&amp;srno=s_1_13&amp;otracker=search&amp;otracker1=search&amp;fm=SEARCH&amp;iid=c7cc3b99-8d53-4d0d-ae95-f87e28b3e0d8.TEAETC92X9NXPDWE.SEARCH&amp;ppt=sp&amp;ppn=sp&amp;qH=65d26c2d2647ea6e">
-         <div class="_25b18c">
-            <div class="_30jeq3">₹140</div>
-            <div class="_3I9_wc">₹165</div>
-            <div class="_3Ay6Sb"><span>15% off</span></div>
-         </div>
-         <div class="_3tcB5a _2hu4Aw">
-            <div>
-               <div class="_2Tpdn3" style="color: rgb(0, 0, 0); font-size: 14px; font-style: normal; font-weight: 400;">Or Pay ₹135 + </div>
-               <div class="CXW8mj _1lT03L" style="height: 12px; width: 12px;"><img class="_396cs4  _3exPp9" alt="" src="https://rukminim1.flixcart.com/www/100/100/promos/18/07/2019/4aebbd99-7478-411e-aced-265e7722d18d.png?q=90"></div>
-               <div class="_2Tpdn3" style="color: rgb(0, 0, 0); font-size: 14px; font-style: normal; font-weight: 400;"> 5</div>
-            </div>
-         </div>
-      </a>
-      <div class="nihHSd">
-         <div class="_3NnGYl"></div>
-         <div>
-            <div class="_gjwbY">
-               More Choices
-               <svg width="16" height="27" viewBox="0 0 16 27" xmlns="http://www.w3.org/2000/svg" class="_1yzl80">
-                  <path d="M16 23.207L6.11 13.161 16 3.093 12.955 0 0 13.161l12.955 13.161z" fill="#2874f0" class=""></path>
-               </svg>
-            </div>
-         </div>
-      </div>
-   </div>
-  </div>
 <div>
 """
 
 from selenium import webdriver
 from bs4 import BeautifulSoup
 import pandas as pd
+import urllib.parse
 
 # Intializing driver
 driver = webdriver.Chrome(executable_path = './bin/chromedriver 2')
 
 # Initialize required list
+pids = []
 names = []
 detail_links = []
 
@@ -117,16 +66,20 @@ soup = BeautifulSoup(content)
 allDiv = soup.select('div[data-id*="NDF"]')
 
 for d in range(len(allDiv)):
-  div = allDiv[d]
-  allA = div.findAll('a',href=True)
+   div = allDiv[d]
+   allA = div.findAll('a',href=True)
+   
+   # @Hardcode [1]
+   name = allA[1].text
+   names.append(name)
+   link = allA[1].get('href')
+   detail_links.append(link)
+   
+   query = urllib.parse.parse_qs(urllib.parse.urlparse(link).query)
+   pid = query['pid']
+   pids += pid
 
-  # @Hardcode [1]
-  name = allA[1].text
-  names.append(name)
-  link = allA[1].get('href')
-  detail_links.append(link)
 
-
-df = pd.DataFrame({'Product Name':names, 'Link': detail_links})
+df = pd.DataFrame({'pid': pids,'Product Name':names, 'Link': detail_links})
 df.to_csv('pro.csv', index=False, encoding='utf-8')
 
