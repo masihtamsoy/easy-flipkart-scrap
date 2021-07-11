0de7f13b38deded55230758d489095c62b7f821f
masihtamsoy <masihtamsoy@gmail.com>
2021-07-12 01:24:19 +0530

    Based on specific class name, its easy to extract info

--- productDetails.py
+++ productDetails.py
@@ -81,83 +81,33 @@ html = """
          </div>
       </div>
    </div>
-   <div class="_1AtVbE col-12-12">
-      <div class="row _2WVRLm">
-         <div class="col col-12-12">
-            <div class="_1PQN-K">
-               <div class="_2taUHM">Delivery</div>
-               <div class="_24N3im">
-                  <div class="_1tgazt _1LO0H9">
-                     <div class="bTgKY7 _16ocF5"></div>
-                     <div class="_2u50Np">
-                        <div class="hivXmn">
-                           <svg width="12" height="12" viewBox="0 0 9 12" class="_1kbGNj" xmlns="http://www.w3.org/2000/svg">
-                              <path fill="#2874f0" class="_6xm1dD" d="M4.2 5.7c-.828 0-1.5-.672-1.5-1.5 0-.398.158-.78.44-1.06.28-.282.662-.44 1.06-.44.828 0 1.5.672 1.5 1.5 0 .398-.158.78-.44 1.06-.28.282-.662.44-1.06.44zm0-5.7C1.88 0 0 1.88 0 4.2 0 7.35 4.2 12 4.2 12s4.2-4.65 4.2-7.8C8.4 1.88 6.52 0 4.2 0z" fill-rule="evenodd"></path>
-                           </svg>
-                           <form autocomplete="off" class="N79-rD"><input type="text" class="_36yFo0" placeholder="Enter Delivery Pincode" value="122001" maxlength="6" id="pincodeInputId"></form>
-                           <div class="_2YsvKq _2ggyDI _16ocF5">
-                              <svg class="_2LJFE8" viewBox="25 25 50 50">
-                                 <circle stroke="#0057e7" class="_2XJHnB" cx="50" cy="50" r="10" fill="none" stroke-width="3" stroke-miterlimit="10"></circle>
-                              </svg>
-                           </div>
-                           <span class="_2P_LDn">
-                              Change
-                              <div class="_2OTVHf _1Hhkx7 BWJsvH  z0fG_Y _1mQK5h _3BB7xg bSp6LQ _3yvRQZ">
-                                 <div class="_3Oikkn _3_ezix _3Hbb-8">Check pincode<button class="_2KpZ6l FYrnUt">✕</button></div>
-                              </div>
-                           </span>
-                        </div>
-                     </div>
-                  </div>
-               </div>
-            </div>
-            <div class="_1SLzzw">
-               <div class="_1OUluu">
-                  <ul class="eWlq4L">
-                     <div class="_20XEBf">
-                        <div class="_3XINqE">
-                           Delivery by<span class="_1TPvTK">11 Jul, Sunday</span><span class="_2-oumo">|</span>
-                           <span class="_2W3miC">
-                              ₹<!-- -->10
-                           </span>
-                           <div class="Bv11UC"><span class="question">?</span></div>
-                        </div>
-                     </div>
-                  </ul>
-               </div>
-               <div class="_2O5gut"><span class="YxlyDn">View Details</span></div>
-            </div>
-         </div>
-      </div>
-   </div>
-   <div class="_1AtVbE col-12-12" style="padding:24px 0px 0px 0px">
-      <div class="_3nkT-2">
-         <div class="_3cFJ8l">Author</div>
-         <div class="_2o-xpa">
-            <a class="_1mXcCf RmoJUa _14Me7y" href="/books/robert-t-kiyosaki~contributor/pr?sid=bks">Robert T. Kiyosaki</a>
-            <div class="_2RbGAi _1Gh8xC">
-               <div class="Dh6Djs"></div>
-            </div>
-         </div>
-         <div class="_2eaE_9 _1Gh8xC">Read More</div>
-      </div>
-   </div>
 </div>
 """
 
-from numpy import product
+import sys
+from os import access
 from selenium import webdriver
 from bs4 import BeautifulSoup
 import pandas as pd
-from bs4 import NavigableString
 import requests
 import unicodedata
+from functools import partial
 
+
+def prepare_info(dirtyStr):
+  s = unicodedata.normalize('NFKD', dirtyStr).encode('ascii', 'ignore')
+  return s.decode("utf-8")
+
+
+def my_end_node(arr, tag):
+   # print ("/////////////////", tag)   
+   # @TODO How to pass different array flog: personal, seller
+   arr.append(prepare_info(tag.text))
+   return True
 
 
 # Intializing driver
 driver = webdriver.Chrome(executable_path = './bin/chromedriver 2')
-driver.get('http://google.com')
 
 
 pids=[]
@@ -173,134 +123,76 @@ for index, row in data.iterrows():
 # print (product_detail_links)
 
 
-# productInfo = {
-#   'name': [],
-#   'rating': [],
-#   'review': [],
-#   'sp': [],
-#   # 'cp': [],
-#   # 'dis': [],
-# }
-
 productInfo = {
   'pid': [],
-  'details': []
+  'name': [],
+  'rate_score': [],
+  'rating_review': [],
+  'sp': [],
+  'cp': [],
+  'dis': [],
+  'top_seller': [],
 }
 
 df = pd.DataFrame(productInfo)
 
 for i in range(len(product_detail_links)):
-  url = "https://www.flipkart.com" + product_detail_links[i]
-  print ("**URL ----> ", url)
+   url = "https://www.flipkart.com" + product_detail_links[i]
+   print ("**URL ----> ", url)
 
-  # response = requests.get(url, headers={'User-Agent': 'Mozilla/5.0'})
-  driver.execute_script("window.open('about:blank', 'secondtab');")
-  driver.switch_to.window("secondtab")
-  driver.get(url)
+   # response = requests.get(url, headers={'User-Agent': 'Mozilla/5.0'})
+   driver.execute_script("window.open('about:blank', 'secondtab');")
+   driver.switch_to.window("secondtab")
+   driver.get(url)
 
-  content = driver.page_source
-  soup = BeautifulSoup(content, "html.parser")
+   content = driver.page_source
+   soup = BeautifulSoup(content, "html.parser")
 
-  # @Hardcode
-  # +3
-  # divs[0] - name, rating, review, sp, sp, dis
-  # divs[4] - seller: name, rating
-  divs = soup.find_all("div", {"class": "_1AtVbE col-12-12"})
-  # print ("**PARENT DIVS ----> ", divs)
+   # import pdb;pdb.set_trace()
+   try:
+      name = soup.find('span', attrs={'class': "B_NuCI"}).text
+      rate_score = soup.find('div', attrs={'class': "_3LWZlK"}).text
+      rating_review = soup.find('span', attrs={'class': "_2_R_DZ"}).text
+      sp = soup.find('div', attrs={'class': "_30jeq3"}).text
+      cp = soup.find('div', attrs={'class': "_3I9_wc"}).text
+      dis = soup.find('div', attrs={'class': "_3Ay6Sb"}).text
+      top_seller = soup.find('div', attrs={'id': "sellerName"}).text
 
-  myArr = []
-  for d in range(5):
-    myArr.append(divs[d].text)
-  
-  print ("myArr===>", pids[i], myArr)
-  df.loc[len(df.index)] = [pids[i], myArr]
+   except AttributeError:
+      print("invalid div class")
+      pass
+   except:
+      print("Unexpected error:", sys.exc_info()[0])
+      raise
+
+   df.loc[len(df.index)] = [
+      pids[i],
+      prepare_info(name),
+      prepare_info(rate_score),
+      prepare_info(rating_review),
+      prepare_info(sp),
+      prepare_info(cp),
+      prepare_info(dis),
+      prepare_info(top_seller)
+   ]
+
 
 df.to_csv('proDetail.csv', index=False, encoding='utf-8')
 
 
-# arr = []
-
-# def end_node(tag):
-#   # print ("/////////////////", tag)
-#   # import pdb;pdb.set_trace()
-  
-#   # @TODO How to pass different array flog: personal, seller
-#   arr.append(tag.text)
-#   return True
-
-
-# # # # Intializing driver
-# # driver = webdriver.Chrome(executable_path = './bin/chromedriver 2')
-
-# # # personalInfoArr = []
-# # # sellerInfoArr = []
-
-# # # @Input
-# # # URL to fetch from Can be looped over / crawled multiple urls
-# # driver.get('https://www.flipkart.com/robert-t-kiyosaki/p/itmbd830a4a7d9a4?pid=RBKG25UHG45PP9FC&lid=LSTRBKG25UHG45PP9FC00ZQUI&marketplace=FLIPKART&q=rich+dad+poor+dad&store=bks&spotlightTagId=BestsellerId_bks&srno=s_1_1&otracker=AS_Query_HistoryAutoSuggest_1_9_na_na_na&otracker1=AS_Query_HistoryAutoSuggest_1_9_na_na_na&fm=SEARCH&iid=221840b7-cd81-401f-af13-0fd1abcaed92.RBKG25UHG45PP9FC.SEARCH&ppt=sp&ppn=sp&ssid=bfvzvdohkw0000001625588523172&qH=abb2c4a032d5632a')
-
-# # content = driver.page_source
-# # soup = BeautifulSoup(content)
-
-# url = "https://www.flipkart.com" + product_detail_links[0]
-# print ("**URL ----> ", url)
-
-# response = requests.get(url, headers={'User-Agent': 'Mozilla/5.0'})
-
-# soup = BeautifulSoup(response.text, "html.parser")
-
+# [@INFO] Wrong intution, there is inconsistency in order of divs
+# For different product divs[3] is not same
 # # @Hardcode
-# # +3
-# # divs[0] - name, rating, review, sp, sp, dis
-# # divs[4] - seller: name, rating
+# # divs[3] - name, rating, review, sp, sp, dis
+# # divs[7] - seller: name, rating
 # divs = soup.find_all("div", {"class": "_1AtVbE col-12-12"})
-# print ("**PARENT DIVS ----> ", divs)
+# # print ("**PARENT DIVS ----> ", divs)
 
-# myArr = []
-# for d in range(len(divs)):
-#   myArr.append(divs[d].text)
+# for d in range(8):
+# if (d in [3, 7]):
+#    myArr = []
+#    end_node = partial(my_end_node, myArr)
+#    divs[d].find_all(end_node)
 
-# personalSoup = divs[3]
-# # sellerSoup = divs[7]
-
-# personalSoup.find_all(end_node)
-# # sellerSoup.find_all(end_node)
-
-# print ("++++++++", arr)
-# # arr = ['Robert T Kiyosaki\xa0\xa0(Paperback, Robert T. Kiyosaki)4.4253 Ratings\xa0&\xa039 Reviews₹120₹39969% off', 'Robert T Kiyosaki\xa0\xa0(Paperback, Robert T. Kiyosaki)', 'Robert T Kiyosaki\xa0\xa0(Paperback, Robert T. Kiyosaki)', 'Robert T Kiyosaki\xa0\xa0(Paperback, Robert T. Kiyosaki)', '4.4253 Ratings\xa0&\xa039 Reviews', '4.4253 Ratings\xa0&\xa039 Reviews', '4.4253 Ratings\xa0&\xa039 Reviews', '4.4', '4.4', '', '253 Ratings\xa0&\xa039 Reviews', '253 Ratings\xa0&\xa039 Reviews', '253 Ratings\xa0', '&', '\xa039 Reviews', '₹120₹39969% off', '₹120₹39969% off', '₹120₹39969% off', '₹120', '₹399', '69% off', '69% off', '', '']
-
-# def prepare_info(dirtyStr):
-#   s = unicodedata.normalize('NFKD', dirtyStr).encode('ascii', 'ignore')
-#   return s.decode("utf-8")
-
-
-
-# productInfo = {
-#   'name': [prepare_info(arr[3])],
-#   'rating': [prepare_info(arr[7])],
-#   'review': [prepare_info(arr[14])],
-#   'sp': [prepare_info(arr[18])],
-#   'cp': [prepare_info(arr[19])],
-#   'dis': [prepare_info(arr[20])],
-# }
-
-# df = pd.DataFrame(productInfo)
-# df.to_csv('proDetail.csv', mode='w', index=False, encoding='utf-8')
-
-# productInfo = {
-#   'name': [],
-#   'rating': [],
-#   'review': [],
-#   'sp': [],
-#   # 'cp': [],
-#   # 'dis': [],
-# }
-
-# df = pd.DataFrame(productInfo)
-
-# # df2 = {'name': 'sdasd', 'rating': 'SO', 'review': '2.3', 'sp': '200'}
-# # df.append(df2, ignore_index = True)
-
-# df.loc[len(df.index)] = ['Amy', 89, 93, 20] 
-
-# df.to_csv('proDetail.csv', index=False, encoding='utf-8')
+#    print ("myArr===>", pids[i], myArr)
+#    df.loc[len(df.index)] = [pids[i], myArr]
